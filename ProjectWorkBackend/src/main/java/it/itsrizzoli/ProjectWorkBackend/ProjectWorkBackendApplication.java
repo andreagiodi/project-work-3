e9cf4010743c5de5326ae641dbe0ac5b5ac8e974
Andrea Giodice <90761154+andreagiodi@users.noreply.github.com>
2025-06-17 10:47:30 +0200

    spring boot init upload

---
+++ ProjectWorkBackend/src/main/java/it/itsrizzoli/ProjectWorkBackend/ProjectWorkBackendApplication.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.ProjectWorkBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkBackendApplication.class, args);
+	}
+
+}
---
+++ ProjectWorkBackend/src/test/java/it/itsrizzoli/ProjectWorkBackend/ProjectWorkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsrizzoli.ProjectWorkBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

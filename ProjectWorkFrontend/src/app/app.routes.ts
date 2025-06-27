fc478eda606b91f256d1cccccc0ec9e06214e8be
Andrea Giodice <90761154+andreagiodi@users.noreply.github.com>
2025-06-27 09:44:34 +0200

    routes init, and home init, to continue

--- ProjectWorkFrontend/src/app/app.routes.ts
+++ ProjectWorkFrontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistrazioneInternoComponent } from './pages/registrazione-interno/registrazione-interno.component';
+import { RegistrazioneEsternoComponent } from './pages/registrazione-esterno/registrazione-esterno.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component:HomeComponent},
+    {path: "register-staff", component:RegistrazioneInternoComponent},
+    {path: "register-ospite", component:RegistrazioneEsternoComponent},
+    {path:"login", component:LoginComponent}
+];

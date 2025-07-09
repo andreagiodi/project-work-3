b51e70f681544245d77a3b3538f36aebc2e06b20
Luc <lucaterra05@gmail.com>
2025-07-09 17:02:26 +0200

    adding StarX logos in medium and small versions. Initiated Router setUp still missing a few elements

--- ProjectWorkFrontend/src/app/app.routes.ts
+++ ProjectWorkFrontend/src/app/app.routes.ts
@@ -2,12 +2,43 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {InternoComponent} from './pages/interno/interno.component';
 import {OspiteComponent} from './pages/ospite/ospite.component';
+import {BenvenutoComponent} from './componenti/benvenuto/benvenuto.component';
+import {LoginFormComponent} from './componenti/benvenuto/childrenComponents/login-form/login-form.component';
+import {
+  RegisterFormInternoComponent
+} from './componenti/benvenuto/childrenComponents/register-form-interno/register-form-interno.component';
+import {
+  RegisterFormEsternoComponent
+} from './componenti/benvenuto/childrenComponents/register-form-esterno/register-form-esterno.component';
 
 export const routes: Routes = [
     {
       path: "",
       component:HomeComponent,
-      children: [],
+      children: [
+        {
+          path: "benvenuto",
+          component: BenvenutoComponent,
+          children: [
+            {
+              path: "login",
+              component: LoginFormComponent,
+            },
+            {
+              path: "registerInterno",
+              component: RegisterFormInternoComponent,
+            },
+            {
+              path: "registerEsterno",
+              component: RegisterFormEsternoComponent,
+            },
+            {
+              path: "**",
+              redirectTo: "benvenuto"
+            }
+          ],
+        }
+      ],
     },
     {
       path: "interno",

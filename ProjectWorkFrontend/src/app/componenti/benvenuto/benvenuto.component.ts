9a7377c1a3293066914ea6530565d6308076fee7
Luc <lucaterra05@gmail.com>
2025-07-10 10:59:23 +0200

    basic example root setup for Routing test, only defnied template bodies for Root, 1st child and 2nd child

--- ProjectWorkFrontend/src/app/componenti/benvenuto/benvenuto.component.ts
+++ ProjectWorkFrontend/src/app/componenti/benvenuto/benvenuto.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-benvenuto',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './benvenuto.component.html',
   styleUrl: './benvenuto.component.css'
 })

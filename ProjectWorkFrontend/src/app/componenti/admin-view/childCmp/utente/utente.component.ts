7876b4f389375afcfa2b09dc53b9054b68648c7b
Nana-04-art <natalia.busuioc@itsrizzoli.it>
2025-07-12 15:13:17 +0200

    creazione hatml e codice di referente - nuovo appuntamento e accetta appuntamento, admin - utente e warning

--- ProjectWorkFrontend/src/app/componenti/admin-view/childCmp/utente/utente.component.ts
+++ ProjectWorkFrontend/src/app/componenti/admin-view/childCmp/utente/utente.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-utente',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    ReactiveFormsModule
   ],
   templateUrl: './utente.component.html',
   styleUrl: './utente.component.css'
 })
 export class UtenteComponent {
+  utente = new FormGroup({
+    ruolo: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+  onSubmit(){
+    console.log(this.utente.value);
+  }
 
 }

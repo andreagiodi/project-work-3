0f57dabc8ad0dff821e7052939bbaabf30cfdc92
Luc <lucaterra05@gmail.com>
2025-07-12 13:50:16 +0200

    fixed router rendering component twice

--- ProjectWorkFrontend/src/app/pages/home/home.component.ts
+++ ProjectWorkFrontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {BenvenutoComponent} from '../../componenti/benvenuto/benvenuto.component'
 
 @Component({
   selector: 'app-homepage',
   imports: [
     RouterOutlet,
-    BenvenutoComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

07700889687035890313ea0b36e9be42f5d6fc04
Nana-04-art <natalia.busuioc@itsrizzoli.it>
2025-07-14 12:15:01 +0200

    aggiunta sfondo alle classi e bottoni

--- ProjectWorkFrontend/src/app/componenti/admin-view/admin-view.component.ts
+++ ProjectWorkFrontend/src/app/componenti/admin-view/admin-view.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {UtentiAdminComponent} from '../utenti-admin/utenti-admin.component';
 
 @Component({
   selector: 'app-admin-view',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    UtentiAdminComponent
   ],
   templateUrl: './admin-view.component.html',
   styleUrl: './admin-view.component.css'

c3da5093844c496b61d63e113e10d67c261c302b
Andrea Giodice <90761154+andreagiodi@users.noreply.github.com>
2025-07-16 17:43:26 +0200

    implemented some methods for dashboard (mainly admin)

--- ProjectWorkFrontend/src/app/services/dashboard-service.service.ts
+++ ProjectWorkFrontend/src/app/services/dashboard-service.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {apiURL} from '../app.config';
 import {BehaviorSubject, Observable, catchError, of, Subscription} from 'rxjs';
 import {FormGroup} from '@angular/forms';
+import { getLocaleDayNames } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,36 @@ export class DashboardService{
 
   createPrenotazione(formPre: FormGroup): Observable<any> {
     return this.http.post(`${apiURL}/prenotazione/create`, {}, { withCredentials: true });
+  }
+
+  getPrenotazioni(): Observable<any> {
+    return this.http.get(`${apiURL}/prenotazione/list`, {withCredentials: true});
+  }
+
+  //ADMIN
+  getOspiti(): Observable<any> {
+    return this.http.get(`${apiURL}/admin/ospiti`, {withCredentials: true});
+  }
+
+  getImpiegati(): Observable<any> {
+    return this.http.get(`${apiURL}/admin/impiegati`, {withCredentials: true});
+  }
+
+  setImpiegatoRuolo(id: number): Observable<any> {
+    return this.http.post(`${apiURL}/admin/impiegati/${id}/ruolo`, {
+      idRuolo: id
+    }, {withCredentials: true})
+  }
+
+  updateImpiegatoPassword(password: number): Observable<any> {
+    return this.http.post(`${apiURL}/admin/impiegati/${password}/password`, {
+      password
+    }, {withCredentials: true})
+  }
+
+  updateOspitePassword(password: string): Observable<any> {
+    return this.http.post(`${apiURL}/admin/ospiti/${password}/password`, {
+      password
+    }, {withCredentials: true})
   }
 }

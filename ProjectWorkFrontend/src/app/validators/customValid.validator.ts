925ed0fd8384fa8bb04e2b9fbaf99d9f30ca5c94
Luc <lucaterra05@gmail.com>
2025-07-16 20:08:21 +0200

    added custom validator for any type of phone number with also spaces in between

--- ProjectWorkFrontend/src/app/validators/customValid.validator.ts
+++ ProjectWorkFrontend/src/app/validators/customValid.validator.ts
@@ -4,7 +4,7 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 /*passwordMatch for regsisterValidation --> VERIFIED WORKS!*/
 export function passwordMatch(): ValidatorFn{
   return (passwords: AbstractControl): ValidationErrors | null =>{
-      const {password, confirm} = passwords.getRawValue();
+      const {password, confirm} = passwords.value;
       return password === confirm ? null : {passwordMissMatch: true};
   }
 }
@@ -13,6 +13,14 @@ export function strongPassword():ValidatorFn{
   return (password:AbstractControl):ValidationErrors |null =>{
     /*validates: min 1 UPPERCASE, min 1 lowercase, min 1 digit, min 1 specialChar*/
     const pswdRegex:RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/;
-    return pswdRegex.test(password.getRawValue()) ? null : {weakPassword: true};
+    return pswdRegex.test(password.value) ? null : {weakPassword: true};
+  }
+}
+
+export function phoneNumber():ValidatorFn{
+  return (telefono: AbstractControl):ValidationErrors  | null =>{
+    /*validates any type of phone numbers ALSO with delimiters such as space, dots, ecc*/
+    const phoneRegex:RegExp = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
+    return phoneRegex.test(telefono.value) ? null : {invalidPhone: true}
   }
 }

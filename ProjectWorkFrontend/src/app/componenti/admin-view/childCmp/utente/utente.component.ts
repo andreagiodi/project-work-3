0724a0b9c34a46bca79f6df25b3587db3c5079a0
Luc <lucaterra05@gmail.com>
2025-07-18 11:33:53 +0200

    small tiny itsy bitty clean of unused imports

--- ProjectWorkFrontend/src/app/componenti/admin-view/childCmp/utente/utente.component.ts
+++ ProjectWorkFrontend/src/app/componenti/admin-view/childCmp/utente/utente.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({

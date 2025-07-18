dec699be74277d3501b5d0a950f6291bbd401224
Luc <lucaterra05@gmail.com>
2025-07-18 10:58:36 +0200

    added redirecting based on idRuolo on landing on interno.component.ts

--- ProjectWorkFrontend/src/app/pages/interno/interno.component.ts
+++ ProjectWorkFrontend/src/app/pages/interno/interno.component.ts
@@ -1,6 +1,7 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {AuthService} from '../../services/auth-service.service';
+import {User} from '../../modelli/user.model';
 
 @Component({
   selector: 'app-interno',
@@ -10,9 +11,39 @@ import {AuthService} from '../../services/auth-service.service';
   templateUrl: './interno.component.html',
   styleUrl: './interno.component.css'
 })
-export class InternoComponent{
+export class InternoComponent implements OnInit {
   router = inject(Router);
   authService = inject(AuthService);
+  currentUser: User | null = null;
+  ngOnInit() {
+    this.currentUser = this.authService.getCurrentUser()
+    this.redirectUser(this.currentUser);
+  }
+
+  redirectUser(user: User | null) {
+    if(user?.userType === 'impiegato')
+      switch (user.idRuolo) {
+        case 1: {
+          this.router.navigate(['interno/receptionist']);
+          break;
+        }
+        case 2: {
+          this.router.navigate(['interno/referente']);
+          break;
+        }
+        case 4: {
+          this.router.navigate(['interno/admin']);
+          break;
+        }
+        default: {
+          this.router.navigate(['benvenuto/login']);
+        }
+      }
+    else{
+      this.router.navigate(['benvenuto/login']);
+    }
+  }
+
   /*logout function call*/
   logOutFunc(){
     this.authService.logout();

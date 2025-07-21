d128ca1e15c5ea396ff151996ccc8e65fe3348c5
Nana-04-art <natalia.busuioc@itsrizzoli.it>
2025-07-21 15:08:22 +0200

    CSS cartella receptionist FINITO

--- ProjectWorkFrontend/src/app/componenti/receptionist-view/receptionist-view.component.ts
+++ ProjectWorkFrontend/src/app/componenti/receptionist-view/receptionist-view.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ProssimiAppuntamentiComponent} from '../prossimi-appuntamenti/prossimi-appuntamenti.component';
+import {StoricoOspiteComponent} from './childCmp/info-ospite/childCmp/storico-ospite/storico-ospite.component';
+import {InfoOspiteComponent} from './childCmp/info-ospite/info-ospite.component';
 @Component({
   selector: 'app-receptionist-view',
   imports: [
     RouterOutlet,
     ProssimiAppuntamentiComponent,
+    StoricoOspiteComponent,
+    InfoOspiteComponent,
   ],
   templateUrl: './receptionist-view.component.html',
   styleUrl: './receptionist-view.component.css'
